7f74021f43ba847fd989f5a956058961906ae2f8
VarunDev2000 <varundev652000@gmail.com>
2020-07-15 14:00:07 +0530

    Thankyou and User verification page completed

--- app/src/main/java/com/dev/startoonapp/Thankyou_page.java
+++ app/src/main/java/com/dev/startoonapp/Thankyou_page.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.threeten.bp.LocalDate;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import org.threeten.bp.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Thankyou_page extends AppCompatActivity {
 
-    Button X;
+    ImageButton X;
+    TextView Date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +30,23 @@ public class Thankyou_page extends AppCompatActivity {
         setContentView(R.layout.activity_thankyou_page);
 
         X = findViewById(R.id.ty_close);
+        Date = findViewById(R.id.date);
+
+        //Date Print
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("hh:mm aa, dd MMM yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+        //Log.d("DATE", "Date -> " + formattedDate);
+        Date.setText(formattedDate);
+
+
 
         X.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(getApplicationContext(),HomePage.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/dev/startoonapp/Verification_Page.java
+++ app/src/main/java/com/dev/startoonapp/Verification_Page.java
@@ -7,10 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class Verification_Page extends AppCompatActivity {
 
-    Button X;
+    ImageButton X;
+    TextView Date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,15 @@ public class Verification_Page extends AppCompatActivity {
         setContentView(R.layout.activity_verification_page);
 
         X = findViewById(R.id.close);
+        Date = findViewById(R.id.v_date);
+
+        //Date Print
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("hh:mm aa, dd MMM yyyy", Locale.getDefault());
+        String formattedDate = df.format(c);
+        //Log.d("DATE", "Date -> " + formattedDate);
+        Date.setText(formattedDate);
+
 
         X.setOnClickListener(new View.OnClickListener() {
             @Override
